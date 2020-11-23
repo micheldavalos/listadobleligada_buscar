f1c8384d4e121caf9bdf6565ec42108157ca3828
Michel Dávalos <michel.davalos@academicos.udg.mx>
2020-11-23 14:22:34 -0600

    agregada la prueba para insertar()

--- main.cpp
+++ main.cpp
@@ -10,28 +10,32 @@ using namespace std;
 int main() {
     ListaDobleLigada<int> enteros;
 
-    enteros.push_front(1);            // insertar al inicio (frente)
-    enteros.push_front(2);            // insertar al inicio (frente)
-    enteros.push_front(3);            // insertar al inicio (frente)
+    enteros.push_front(1);           // insertar al inicio (frente)
+    enteros.push_front(2);           // insertar al inicio (frente)
+    enteros.push_front(3);           // insertar al inicio (frente)
 
-    enteros << 4 << 5 << 6;           // insertar al final (cola)
+    enteros << 4 << 5 << 6;          // insertar al final (cola)
 
-    cout << enteros.size() << endl;   // imprime la cantidad de nodos (elementos)
-    
-    enteros.print();                  // imprime el elemento de cada nodo
+    cout << enteros.size() << endl;  // imprime la cantidad de nodos (elementos)
 
-    int *ptr_01 = enteros.buscar(1);  // buscar un elemento
-    if (ptr_01) {                     // si no es nulo
-        *ptr_01 = 3;                  // cambiar valor
+    enteros.insert(0, 1);            // insertar el 0 después del primer nodo
+
+    enteros.print();                 // imprime el elemento de cada nodo
+
+    int *ptr_01 = enteros.buscar(1); // buscar un elemento
+    if (ptr_01) {                    // si no es nulo
+        *ptr_01 = 3;                 // cambiar valor
     }
 
-    int *ptr_02 = enteros.buscar(3);  // buscar un elemento
-    if (ptr_02) {                     // si no es nulo
-        *ptr_02 = 1;                  // cambiar valor
+    int *ptr_02 = enteros.buscar(3); // buscar un elemento
+    if (ptr_02) {                    // si no es nulo
+        *ptr_02 = -1;                // cambiar valor
     }
 
     cout << enteros.size() << endl;  // imprime la cantidad de nodos (elementos)
-    
+
+    enteros.erase(5);                // elimina el sexto nodo (penultimo de la lista de enteros)
+
     enteros.print();                 // imprime el elemento de cada nodo
     
     return 0;
